aad80e78641aa29de7fb8864bb6adb7a1d89704d
Ibrahim Halil Altun <ihalilaltun@gmail.com>
2024-01-03 15:00:32 +0300

    Update config.ts
    update example url

--- examplenativeapp/config.ts
+++ examplenativeapp/config.ts
@@ -2,7 +2,7 @@ export const providerConfig = {
   config: {
     apiKey: 'dd44a4c1-9d95-4ad6-8ff9-090c85f88113',
     dataCenterUrl: 'https://gandalf-qa.segmentify.com',
-    subDomain: 'https://www.bauhaus.com.tr/',
+    subDomain: 'https://www.example.com/',
     dataCenterPushUrl: 'https://gimli-qa.segmentify.com',
     isApnsEnabled: false,
   },
