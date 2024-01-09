c83c596d64c6026f02448dde322e81da5873acd8
Hasan Ugur <hasan.ugur@segmentify.com>
2024-01-09 14:49:50 +0300

    feat: update example app configs and segmentify package version

--- examplenativeapp/config.ts
+++ examplenativeapp/config.ts
@@ -1,10 +1,11 @@
 export const providerConfig = {
   config: {
-    apiKey: 'dd44a4c1-9d95-4ad6-8ff9-090c85f88113',
+    apiKey: '601a3084-d02b-4e78-a474-51ac38342958',
     dataCenterUrl: 'https://gandalf-qa.segmentify.com',
-    subDomain: 'https://www.bauhaus.com.tr/',
+    subDomain: 'https://www.mototas.com.tr/',
     dataCenterPushUrl: 'https://gimli-qa.segmentify.com',
     isApnsEnabled: false,
+    language: 'TR',
   },
   user: {
     userId: '',
