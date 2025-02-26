6860fee94506c68201da962c0289c9dd56063a29
sarpkayature <sarpkaya.ture@segmentify.com>
2025-02-26 10:08:11 +0300

    chore: remove unused assets and configuration files

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,22 +1,24 @@
-const path = require('path');
-const pak = require('../package.json');
-
-module.exports = function (api) {
-  api.cache(true);
-
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          extensions: ['.tsx', '.ts', '.js', '.json'],
-          alias: {
-            // For development, we want to alias the library to the source
-            [pak.name]: path.join(__dirname, '..', pak.source),
-          },
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        alias: {
+          '@segmentify/react-native-sdk': ['../src/index.js'],
         },
-      ],
+      },
     ],
-  };
+  ],
 };
--- example/src/router/Router.tsx
+++ example/src/router/Router.tsx
@@ -5,7 +5,7 @@ import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 // Screens
-import {Home, Product, Search} from '../screens/';
+import {Home, Product, Search} from '../screens';
 
 // Components
 import {SearchBar} from '../components/index';
